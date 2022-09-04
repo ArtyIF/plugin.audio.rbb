d10a539335a13b8a02e046b30f5fe271bda64f81
ArtyIF <artyomisflash@mail.ru>
2022-09-04 12:36:55 +0300

    Improved error handling in locales

--- resources/lib/locale.py
+++ resources/lib/locale.py
@@ -9,7 +9,7 @@ ADDON = xbmcaddon.Addon()
 # Built-in strings from:
 # https://github.com/xbmc/xbmc/blob/master/addons/resource.language.en_gb/resources/strings.po
 LOCALIZED_STRINGS = {
-    19140: "Search...", # TODO: check if this the string that's needed
+    19140: "Search...",
     30000: "RadioBrowser² internal error! Tell the developer!",
     30001: "Add-on tried to access a non-existent mode: {0} (all arguments: {1})",
     30002: "Page {0}",
@@ -18,9 +18,14 @@ LOCALIZED_STRINGS = {
 
 
 def localize_context(str_id):
+    result = ""
     if 30000 <= str_id < 33000:
-        return ADDON.getLocalizedString(str_id)
-    return xbmc.getLocalizedString(str_id)
+        result = ADDON.getLocalizedString(str_id)
+    else:
+        result = xbmc.getLocalizedString(str_id)
+    if len(result) == 0:
+        raise KeyError(str_id)
+    return result
 
 
 def localize_string(unlocalized_string):
@@ -31,4 +36,6 @@ def localize_string(unlocalized_string):
             ]
         )
     except ValueError:
-        return f"MISSING IN LOCALIZED_STRINGS: {unlocalized_string}"
+        return f"[B][COLOR red]MISSING IN LOCALIZED_STRINGS:[/COLOR][/B] {unlocalized_string}"
+    except KeyError:
+        return f"[B][COLOR red]MISSING IN PO FILE:[/COLOR][/B] {unlocalized_string}"
